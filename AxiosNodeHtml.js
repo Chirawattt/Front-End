3099c2e3d490dff598044f70f60d19d901dc9627
Chirawattt <chirawat.yana@hotmail.com>
2024-02-14 10:30:46 +0700

    update path

--- AxiosNodeHtml.js
+++ AxiosNodeHtml.js
@@ -9,11 +9,11 @@ const app = express();
 
 // Base URL for the API
 // const base_url = 'http://api.example.com';
-// const base_url = 'http://localhost:3000';
-const base_url = 'http://10.104.16.253:3000';
+const base_url = 'http://localhost:3000';
+// const base_url = 'http://10.104.16.253:3000';
 
 // Set the template engine
-app.set("views",path.join(__dirname, "/public/views"));
+app.set("views", path.join(__dirname, "./public/views"));
 app.set('view engine', 'ejs');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
