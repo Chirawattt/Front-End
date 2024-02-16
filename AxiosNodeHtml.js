f07f676e3ccce59a5d515ab24345974763284354
Chirawattt <chirawat.yana@hotmail.com>
2024-02-16 21:59:14 +0700

    update wrongpath on line 16

--- AxiosNodeHtml.js
+++ AxiosNodeHtml.js
@@ -13,7 +13,7 @@ const app = express();
 const base_url = 'http://node56983-chirawat-noderest.proen.app.ruk-com.cloud';
 
 // Set the template engine
-app.set("views", path.join(__dirname, "../public/views"));
+app.set("views", path.join(__dirname, "./public/views"));
 app.set('view engine', 'ejs');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
